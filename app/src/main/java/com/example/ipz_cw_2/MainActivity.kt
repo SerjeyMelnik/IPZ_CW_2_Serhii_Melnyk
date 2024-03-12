4e9939a1e6ca80ae7722dc733b17e080bbc21ba5
Serhii Melnyk <serjeymelnik@gmail.com>
2024-03-12 14:45:17 +0200

    realize navigation

--- app/src/main/java/com/example/ipz_cw_2/MainActivity.kt
+++ app/src/main/java/com/example/ipz_cw_2/MainActivity.kt
@@ -8,18 +8,16 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -32,20 +30,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent { Navigation() }
-        setContent {
-            IPZ_CW_2_Serhii_MelnykTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    SignIn(
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
     }
 }
 
 @Composable
-fun SignIn(modifier: Modifier = Modifier) {
+fun SignIn(modifier: Modifier = Modifier, navController: NavController) {
 
     Column {
         TextField(value = "", onValueChange = {}, placeholder = { Text("Enter Email") })
@@ -63,13 +52,13 @@ fun SignIn(modifier: Modifier = Modifier) {
 @Composable
 fun SignInPreview() {
     IPZ_CW_2_Serhii_MelnykTheme {
-        SignIn()
+        SignIn(navController = NavController(LocalContext.current))
     }
 }
 
 
 @Composable
-fun DetailScreen() {
+fun DetailScreen(modifier: Modifier = Modifier, navController: NavController) {
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -77,15 +66,15 @@ fun DetailScreen() {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-       //TODO make success body
+        //TODO make success body
     }
 }
 
 @Preview
 @Composable
-fun DetailScreenPreview(){
+fun DetailScreenPreview() {
     IPZ_CW_2_Serhii_MelnykTheme {
-        DetailScreen()
+        DetailScreen(navController = NavController(LocalContext.current))
     }
 }
 
@@ -94,20 +83,16 @@ fun DetailScreenPreview(){
 fun Navigation() {
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = Screen.MainScreen.route) {
-//        composable(route = Screen.MainScreen.route) {
-//            Screen.MainScreen(navController = navController)
-//        }
-//        composable(
-//            route = Screen.DetailScreen.route + "/{name}",
-//            arguments = listOf(
-//                navArgument("name") {
-//                    type = NavType.StringType
-//                    defaultValue = "Some Default"
-//                    nullable = true
-//                }
-//            )
-//        ) { entry ->
-//            Screen.DetailScreen(name = entry.arguments?.getString("name"))
-//        }
+        composable(route = Screen.MainScreen.route) {
+            SignIn(navController = navController)
+        }
+        composable(route = Screen.DetailScreen.route + "/{name}",
+            arguments = listOf(navArgument("name") {
+                type = NavType.StringType
+                defaultValue = "Some Default"
+                nullable = true
+            })) { entry ->
+            DetailScreen(navController = navController)
+        }
     }
 }
