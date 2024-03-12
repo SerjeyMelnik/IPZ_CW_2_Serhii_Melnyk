bd56d9492bbc99965b35db5fc4644176ad3548fc
Serhii Melnyk <serjeymelnik@gmail.com>
2024-03-12 14:21:24 +0200

    first screen

--- app/src/main/java/com/example/ipz_cw_2/MainActivity.kt
+++ app/src/main/java/com/example/ipz_cw_2/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -20,8 +19,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             IPZ_CW_2_Serhii_MelnykTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    SignIn(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +29,14 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun SignIn(modifier: Modifier = Modifier) {
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun SignInPreview() {
     IPZ_CW_2_Serhii_MelnykTheme {
-        Greeting("Android")
+        SignIn()
     }
 }
