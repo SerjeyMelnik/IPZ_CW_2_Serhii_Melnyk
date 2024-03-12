869e3373fe2fc391d9d62a5a55a32698ad1231d7
Serhii Melnyk <serjeymelnik@gmail.com>
2024-03-12 14:27:31 +0200

    add sign in

--- app/src/main/java/com/example/ipz_cw_2/MainActivity.kt
+++ app/src/main/java/com/example/ipz_cw_2/MainActivity.kt
@@ -4,10 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.ipz_cw_2.ui.theme.IPZ_CW_2_Serhii_MelnykTheme
@@ -30,6 +35,16 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun SignIn(modifier: Modifier = Modifier) {
+
+    Column {
+        TextField(value = "", onValueChange = {}, placeholder = { Text("Enter Email") })
+        TextField(value = "", onValueChange = {}, placeholder = { Text("Enter password") })
+        Button(onClick = {
+
+        }) {
+            Text(text = "Sign in")
+        }
+    }
 
 }
 
